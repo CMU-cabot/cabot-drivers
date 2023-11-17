aea0b2ef24e0d27050aac3752d8e68bcc9a4d983
Daisuke Sato <daisukes@cmu.edu>
2023-11-17 12:10:22 -0500

    add build script and improve docker build configurations
    Signed-off-by: Daisuke Sato <daisukes@cmu.edu>

---
+++ build-docker.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+# Copyright (c) 2020, 2023  Carnegie Mellon University, IBM Corporation, and others
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+## termination hook
+trap ctrl_c INT QUIT TERM
+
+function ctrl_c() {
+    exit
+}
+
+source ./cabot-common/build-utils.sh
+
+function help {
+    echo "Usage: $0 <option>"
+    echo "$0 [<option>] [<target>]"
+    echo ""
+    echo "-h                    show this help"
+    echo "-n                    no cache option to build docker image"
+    echo "-t <time_zone>        set time zone"
+    echo "-u <uid>              replace uid"
+    echo "-p                    prebuild images"
+    echo "-P <prefix>           prebuild with prefix"
+    echo "-i                    build images"
+    echo "-w                    build workspace"
+
+	echo "Available services:"
+    show_available_services dcfiles
+}
+
+pwd=$(pwd)
+scriptdir=$(dirname $0)
+cd $scriptdir
+scriptdir=$(pwd)
+prefix=$(basename $scriptdir)
+
+option="--progress=auto"
+time_zone=$(cat /etc/timezone)
+uid=$UID
+prebuild=0
+build_image=0
+build_workspace=0
+
+export DOCKER_BUILDKIT=1
+export COMPOSE_DOCKER_CLI_BUILD=1
+
+while getopts "hnt:u:pP:iw" arg; do
+    case $arg in
+	h)
+	    help
+	    exit
+	    ;;
+	n)
+	    option="$option --no-cache"
+	    ;;
+	t)
+	    time_zone=$OPTARG
+	    ;;
+    u)
+        uid=$OPTARG
+        ;;
+    p)
+        prebuild=1
+        ;;
+    P)
+        prebuild=1
+        prefix=${OPTARG}
+        ;;
+    i)
+        build_image=1
+        ;;
+	w)
+	    build_workspace=1
+	    ;;
+    esac
+done
+shift $((OPTIND-1))
+targets=$@
+
+if [[ $prebuild -eq 1 ]]; then
+    ./cabot-common/prebuild_docker.sh -P $prefix
+    if [ $? != 0 ]; then exit 1; fi
+fi
+
+readarray -t dcfiles < <(ls docker-compose* | grep -v jetson | grep -v vs)
+
+if [[ $build_image -eq 1 ]]; then
+    build_image dcfiles targets option time_zone uid prefix
+    if [ $? != 0 ]; then exit 1; fi
+fi
+
+if [[ $build_workspace -eq 1 ]]; then
+    build_workspace dcfiles targets
+    if [ $? != 0 ]; then exit 1; fi
+fi
+
+if [[ $prebuild -eq 0 ]] && [[ $build_image -eq 0 ]] && [[ $build_workspace -eq 0 ]]; then
+	help
+fi
---
+++ script/launch_driver.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+
+# Copyright (c) 2020, 2023  Carnegie Mellon University
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# change directory to where this script exists
+pwd=`pwd`
+scriptdir=`dirname $0`
+cd $scriptdir
+scriptdir=`pwd`
+
+pids=()
+termpids=()
+checks=()
+
+## debug
+debug=0
+command_prefix=''
+command_postfix='&'
+
+# load utility functions
+source $scriptdir/cabot_util.sh
+
+trap signal INT TERM
+
+function signal() {
+    blue "trap cabot_ros2.sh "
+
+    # ps -Af
+    kill -INT -1
+    for pid in ${termpids[@]}; do
+	kill -TERM $pid
+    done
+    for pid in ${pids[@]}; do
+	count=0
+        while kill -0 $pid 2> /dev/null; do
+	    if [[ $count -eq 15 ]]; then
+		blue "escalate to SIGTERM $pid"
+		com="kill -TERM $pid"
+		eval $com
+	    fi
+	    if [[ $count -eq 30 ]]; then
+		blue "escalate to SIGKILL $pid"
+		com="kill -KILL $pid"
+		eval $com
+	    fi
+            echo "waiting $0 $pid"
+	    # ps -Af
+            snore 1
+	    count=$((count+1))
+        done
+    done
+    
+    exit
+}
+
+# initialize environment variables
+# required variables
+: ${CABOT_SIDE:=left}
+: ${CABOT_MODEL:=}
+: ${CABOT_TOUCH_PARAMS:=}
+
+
+# check required environment variables
+error_flag=0
+if [[ -z $CABOT_MODEL ]]; then
+    err "CABOT_MODEL should be configured"
+    error_flag=1
+fi
+if [[ -z $CABOT_TOUCH_PARAMS ]]; then
+    err "CABOT_TOUCH_PARAMS should be configured"
+    error_flag=1
+fi
+if [[ $error_flag -ne 0 ]]; then
+    exit
+fi
+
+source $scriptdir/../install/setup.bash
+
+# check cabot major version to switch venv and launch file
+cabot_major=${CABOT_MODEL:0:6} # cabotN
+venv_path=/opt/venv/$cabot_major/bin/activate
+cabot_launch_py=$cabot_major.launch.py
+
+blue "bringup $CABOT_MODEL base"
+com="$command_prefix source $venv_path && \
+        ros2 launch cabot_base $cabot_launch_py $command_postfix"
+echo $com
+eval $com
+checks+=($!)
+pids+=($!)
+
+## wait until it is terminated by the user
+while [ 1 -eq 1 ];
+do
+    # blue "snore"
+    for pid in ${checks[@]}; do
+        kill -0 $pid 2> /dev/null
+	if [[ $? -ne 0 ]]; then
+	    red "process (pid=$pid) is not running, please check logs"
+	    exit
+	fi
+    done
+    snore 1
+done
