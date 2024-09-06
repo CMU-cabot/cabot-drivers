da485c347db0c43ee4a3973ba81fbde2e71e93e5
yushi-minemura <67675657+yushi-minemura@users.noreply.github.com>
2024-09-06 11:32:55 +0900

    replace part of the processing with the ODriveManager class
    Signed-off-by: yushi-minemura <67675657+yushi-minemura@users.noreply.github.com>

--- motor_controller/odriver_can_adapter/src/odriver_can_adapter_node.cpp
+++ motor_controller/odriver_can_adapter/src/odriver_can_adapter_node.cpp
@@ -47,11 +47,6 @@ public:
   {
     using std::placeholders::_1;
 
-    axis_state_left_info_.is_ready_axis_state_service = true;
-    axis_state_left_info_.name = "left";
-    axis_state_right_info_.is_ready_axis_state_service = true;
-    axis_state_right_info_.name = "right";
-
     this->declare_parameter("wheel_diameter_m", 0.17);
     wheel_diameter_m_ = this->get_parameter("wheel_diameter_m").as_double();
     meter_per_round_ = wheel_diameter_m_ * M_PI;
@@ -65,46 +60,29 @@ public:
     this->declare_parameter("service_timeout_ms", 2000);
     service_timeout_ms_ = this->get_parameter("service_timeout_ms").as_int();
 
+    double sign_left, sign_right;
     if(is_clockwise_) {
-      sign_left_ = -1.0;
-      sign_right_ = 1.0;
+      sign_left = -1.0;
+      sign_right = 1.0;
     } else {
-      sign_left_ = 1.0;
-      sign_right_ = -1.0;
+      sign_left = 1.0;
+      sign_right = -1.0;
     }
-
-    rclcpp::QoS control_message_left_qos(rclcpp::KeepLast(10));
-    control_message_left_pub_ = create_publisher<odrive_can::msg::ControlMessage>(
-                                  "/control_message_left",
-                                  control_message_left_qos);
-
-    rclcpp::QoS control_message_right_qos(rclcpp::KeepLast(10));
-    control_message_right_pub_ = create_publisher<odrive_can::msg::ControlMessage>(
-                                  "/control_message_right",
-                                  control_message_right_qos);
+    odrive_left_ = std::make_shared<ODriveManager>(
+                      this,
+                      "left",
+                      sign_left,
+                      wheel_diameter_m_);
+    odrive_right_ = std::make_shared<ODriveManager>(
+                      this,
+                      "right",
+                      sign_right,
+                      wheel_diameter_m_);
 
     rclcpp::QoS motor_status_qos(rclcpp::KeepLast(10));
     motor_status_pub_ = create_publisher<odriver_msgs::msg::MotorStatus>(
                                   "/motor_status",
                                   motor_status_qos);
-
-    rclcpp::QoS controller_status_left_qos(rclcpp::KeepLast(10));
-    controller_status_left_sub_ = create_subscription<odrive_can::msg::ControllerStatus>(
-                                    "/controller_status_left",
-                                    controller_status_left_qos,
-                                    std::bind(
-                                      &ODriverCanAdapterNode::controllerStatusLeftCallback,
-                                      this,
-                                      _1));
-
-    rclcpp::QoS controller_status_right_qos(rclcpp::KeepLast(10));
-    controller_status_right_sub_ = create_subscription<odrive_can::msg::ControllerStatus>(
-                                    "/controller_status_right",
-                                    controller_status_right_qos,
-                                    std::bind(
-                                      &ODriverCanAdapterNode::controllerStatusRightCallback,
-                                      this,
-                                      _1));
 
     rclcpp::QoS motor_target_qos(rclcpp::KeepLast(10));
     motor_target_sub_ = create_subscription<odriver_msgs::msg::MotorTarget>(
@@ -114,11 +92,6 @@ public:
                             &ODriverCanAdapterNode::motorTargetCallback,
                             this,
                             _1));
-
-    axis_state_left_info_.client =
-      create_client<odrive_can::srv::AxisState>("/request_axis_state_left");
-    axis_state_right_info_.client =
-      create_client<odrive_can::srv::AxisState>("/request_axis_state_right");
 
     timer_ = create_wall_timer(
               std::chrono::seconds(1) / hz_,
@@ -131,72 +104,18 @@ public:
   {
   }
 private:
-  void controllerStatusLeftCallback(const odrive_can::msg::ControllerStatus::SharedPtr msg)
-  {
-    spd_left_c_ = msg->vel_estimate;
-    dist_left_c_ = msg->pos_estimate;
-    current_setpoint_left_ = msg->iq_setpoint;
-    current_measured_left_ = msg->iq_measured;
-    axis_state_left_info_.axis_state = msg->axis_state;
-  }
-
-  void controllerStatusRightCallback(const odrive_can::msg::ControllerStatus::SharedPtr msg)
-  {
-    spd_right_c_ = msg->vel_estimate;
-    dist_right_c_ = msg->pos_estimate;
-    current_setpoint_right_ = msg->iq_setpoint;
-    current_measured_right_ = msg->iq_measured;
-    axis_state_right_info_.axis_state = msg->axis_state;
-  }
-
-  void callAxisStateService(unsigned int axis_state, AxisStateClientInfo *info)
-  {
-    using AxisState = odrive_can::srv::AxisState;
-    using AxisStateClient = rclcpp::Client<AxisState>;
-    using std::chrono::system_clock;
-
-    AxisState::Request::SharedPtr request = std::make_shared<AxisState::Request>();
-    request->axis_requested_state = axis_state;
-
-    if(info->is_ready_axis_state_service) {
-      RCLCPP_INFO(
-        get_logger(),
-        "call axis_state_%s service to %d mode from %d mode",
-        info->name.c_str(),
-        info->axis_state,
-        axis_state);
-      while(!info->client->wait_for_service(std::chrono::seconds(1)) &&
-              rclcpp::ok()) {
-        RCLCPP_INFO(
-          get_logger(),
-          "request_axis_state_%s service not available, waiting again...",
-          info->name.c_str());
-      }
-
-      info->is_ready_axis_state_service = false;
-      info->last_call_time = system_clock::now();
-      AxisStateClient::SharedFutureWithRequestAndRequestId axis_state_client_future_ =
-        info->client->async_send_request(
-          request,
-          [info](AxisStateClient::SharedFutureWithRequest future) {
-            (void) future;
-            info->is_ready_axis_state_service = true;
-          });
-    }
-  }
-
   // change odrive axis.controller.config.control_mode
   // closed_loop: true -> CLOSED_LOOP_CONTROL, false -> IDOL
   void changeAxisState(bool closed_loop)
   {
     if(closed_loop &&
-          (axis_state_left_info_.axis_state == kAxisStateIdol || axis_state_right_info_.axis_state == kAxisStateIdol)) {
-      callAxisStateService(kAxisStateClosedLoopControl, &axis_state_left_info_);
-      callAxisStateService(kAxisStateClosedLoopControl, &axis_state_right_info_);
+          (odrive_left_->getAxisState() == kAxisStateIdol || odrive_right_->getAxisState() == kAxisStateIdol)) {
+      odrive_left_->callAxisStateService(kAxisStateClosedLoopControl);
+      odrive_right_->callAxisStateService(kAxisStateClosedLoopControl);
     } else if(!closed_loop &&
-                (axis_state_left_info_.axis_state == kAxisStateClosedLoopControl || axis_state_right_info_.axis_state == kAxisStateClosedLoopControl)) {
-      callAxisStateService(kAxisStateIdol, &axis_state_left_info_);
-      callAxisStateService(kAxisStateIdol, &axis_state_right_info_);
+                (odrive_left_->getAxisState() == kAxisStateClosedLoopControl || odrive_right_->getAxisState() == kAxisStateClosedLoopControl)) {
+      odrive_left_->callAxisStateService(kAxisStateIdol);
+      odrive_right_->callAxisStateService(kAxisStateIdol);
     }
   }
 
@@ -207,73 +126,54 @@ private:
     // !msg->loop_ctrl is IDOL mode
     if(!msg->loop_ctrl) { return; }
 
-    odrive_can::msg::ControlMessage left_message;
-    odrive_can::msg::ControlMessage right_message;
-    
-    // set velocity mode
-    left_message.control_mode = kVelocityControlMode;
-    right_message.control_mode = kVelocityControlMode;
+    odrive_left_->publishControlMessage(
+                    kVelocityControlMode,
+                    kPassthroughInputMode,
+                    msg->spd_left);
 
-    left_message.input_mode = kPassthroughInputMode;
-    right_message.input_mode = kPassthroughInputMode;
-
-    // meter/sec -> rotation/sec
-    left_message.input_vel = sign_left_ * msg->spd_left / meter_per_round_;
-    right_message.input_vel = sign_right_ * msg->spd_right / meter_per_round_;
-
-    if(axis_state_left_info_.is_ready_axis_state_service) {
-      control_message_left_pub_->publish(left_message);
-    }
-    if(axis_state_right_info_.is_ready_axis_state_service) {
-      control_message_right_pub_->publish(right_message);
-    }
+    odrive_right_->publishControlMessage(
+                    kVelocityControlMode,
+                    kPassthroughInputMode,
+                    msg->spd_right);
   }
 
   void timerCallback()
   {
-    using Milliseconds = std::chrono::milliseconds;
-    using Seconds = std::chrono::seconds;
-    using std::chrono::system_clock;
-    // check timeout ready flag that request_axis_state service
-    double left_diff_time =
-      std::chrono::duration_cast<Milliseconds>(axis_state_left_info_.last_call_time - system_clock::now()).count();
-    if(!axis_state_left_info_.client->wait_for_service(Seconds(0)) &&
-        left_diff_time >= service_timeout_ms_) {
+    odrive_left_->checkTimeoutServiceResponse(service_timeout_ms_);
+    odrive_right_->checkTimeoutServiceResponse(service_timeout_ms_);
 
-      axis_state_left_info_.client->prune_pending_requests();
-      axis_state_left_info_.is_ready_axis_state_service = true;
-    }
-
-    double right_diff_time =
-      std::chrono::duration_cast<Milliseconds>(axis_state_right_info_.last_call_time - system_clock::now()).count();
-    if(!axis_state_right_info_.client->wait_for_service(Seconds(0)) &&
-        right_diff_time >= service_timeout_ms_) {
-
-      axis_state_right_info_.client->prune_pending_requests();
-      axis_state_right_info_.is_ready_axis_state_service = true;
-    }
+    double sign_left = odrive_left_->getSign();
+    double sign_right = odrive_right_->getSign();
+    double dist_left_c = odrive_left_->getDistC();
+    double dist_right_c = odrive_right_->getDistC();
+    double spd_left_c = odrive_left_->getSpdC();
+    double spd_right_c = odrive_right_->getSpdC();
+    double current_setpoint_left = odrive_left_->getCurrentSetpoint();
+    double current_setpoint_right = odrive_right_->getCurrentSetpoint();
+    double current_measured_left = odrive_left_->getCurrentMeasured();
+    double current_measured_right = odrive_right_->getCurrentMeasured();
 
     odriver_msgs::msg::MotorStatus status;
 
     status.header.stamp = this->get_clock()->now();
 
-    status.dist_left_c = sign_left_ * dist_left_c_;
-    status.dist_right_c = sign_right_ * dist_right_c_;
+    status.dist_left_c = sign_left * dist_left_c;
+    status.dist_right_c = sign_right * dist_right_c;
 
-    status.spd_left_c = sign_left_ * spd_left_c_;
-    status.spd_right_c = sign_right_ * spd_right_c_;
+    status.spd_left_c = sign_left * spd_left_c;
+    status.spd_right_c = sign_right * spd_right_c;
 
-    status.dist_left = sign_left_ * dist_left_c_ * meter_per_round_;
-    status.dist_right = sign_right_ * dist_right_c_ * meter_per_round_;
+    status.dist_left = sign_left * dist_left_c * meter_per_round_;
+    status.dist_right = sign_right * dist_right_c * meter_per_round_;
 
-    status.spd_left = sign_left_ * spd_left_c_ * meter_per_round_;
-    status.spd_right = sign_right_ * spd_right_c_ * meter_per_round_;
+    status.spd_left = sign_left * spd_left_c * meter_per_round_;
+    status.spd_right = sign_right * spd_right_c * meter_per_round_;
 
-    status.current_setpoint_left = sign_left_ * current_setpoint_left_;
-    status.current_setpoint_right = sign_right_ * current_setpoint_right_;
+    status.current_setpoint_left = sign_left * current_setpoint_left;
+    status.current_setpoint_right = sign_right * current_setpoint_right;
 
-    status.current_measured_left = sign_left_ * current_measured_left_;
-    status.current_measured_right = sign_right_ * current_measured_right_;
+    status.current_measured_left = sign_left * current_measured_left;
+    status.current_measured_right = sign_right * current_measured_right;
 
     motor_status_pub_->publish(status);
   }
@@ -295,33 +195,14 @@ private:
 
   double meter_per_round_;
 
-  double spd_left_c_;
-  double spd_right_c_;
-
-  double dist_left_c_;
-  double dist_right_c_;
-
-  double current_setpoint_left_;
-  double current_setpoint_right_;
-
-  double current_measured_left_;
-  double current_measured_right_;
-
-  double sign_left_;
-  double sign_right_;
-
-  rclcpp::Publisher<odrive_can::msg::ControlMessage>::SharedPtr control_message_left_pub_;
-  rclcpp::Publisher<odrive_can::msg::ControlMessage>::SharedPtr control_message_right_pub_;
   rclcpp::Publisher<odriver_msgs::msg::MotorStatus>::SharedPtr motor_status_pub_;
 
-  rclcpp::Subscription<odrive_can::msg::ControllerStatus>::SharedPtr controller_status_left_sub_;
-  rclcpp::Subscription<odrive_can::msg::ControllerStatus>::SharedPtr controller_status_right_sub_;
   rclcpp::Subscription<odriver_msgs::msg::MotorTarget>::SharedPtr motor_target_sub_;
 
-  AxisStateClientInfo axis_state_left_info_;
-  AxisStateClientInfo axis_state_right_info_;
-
   rclcpp::TimerBase::SharedPtr timer_;
+
+  std::shared_ptr<ODriveManager> odrive_left_;
+  std::shared_ptr<ODriveManager> odrive_right_;
 };
 
 int main(int argc, char* argv[])
