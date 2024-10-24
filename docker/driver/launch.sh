10c58070f5bc2ff401ee773298860140941b3f33
yushi-minemura <67675657+yushi-minemura@users.noreply.github.com>
2024-10-24 14:59:10 +0900

    fix license information
    Signed-off-by: yushi-minemura <67675657+yushi-minemura@users.noreply.github.com>

--- docker/driver/launch.sh
+++ docker/driver/launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # ******************************************************************************
-#  Copyright (c) 2024  Carnegie Mellon University and Miraikan
+#  Copyright (c) 2023, 2024  Carnegie Mellon University and Miraikan
 #
 #  Permission is hereby granted, free of charge, to any person obtaining a copy
 #  of this software and associated documentation files (the "Software"), to deal
