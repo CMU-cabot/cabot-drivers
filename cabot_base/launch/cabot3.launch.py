45851f3b588aa713bda1c092c90fb5a36e4cfe16
Daisuke Sato <daisukes@cmu.edu>
2025-06-05 01:10:31 -0400

    fix parameters, docker volume locations
    Signed-off-by: Daisuke Sato <daisukes@cmu.edu>

--- cabot_base/launch/cabot3.launch.py
+++ cabot_base/launch/cabot3.launch.py
@@ -613,7 +613,7 @@ def generate_launch_description():
                 namespace='/',
                 name='uwbtag_publisher_1',
                 output=output,
-                parameters=[
+                parameters=[*param_files,
                     {
                         'frame_id': 'uwb_tag1_link',
                     }
@@ -628,7 +628,7 @@ def generate_launch_description():
                 namespace='/',
                 name='uwbtag_publisher_2',
                 output=output,
-                parameters=[
+                parameters=[*param_files,
                     {
                         'frame_id': 'uwb_tag2_link',
                     }
@@ -643,7 +643,7 @@ def generate_launch_description():
                 namespace='/',
                 name='uwbtag_publisher_3',
                 output=output,
-                parameters=[
+                parameters=[*param_files,
                     {
                         'frame_id': 'uwb_tag3_link',
                     }
